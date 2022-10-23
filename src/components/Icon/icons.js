8fe550f68e7b8130e2a1329e7625dec798a196f3
Tatiana Roslyakova <tat-rs@yandex.ru>
2022-10-23 13:26:43 +0300

    feat: создать 1 слайд

--- src/components/Icon/icons.js
+++ src/components/Icon/icons.js
@@ -1,20 +1,9 @@
 import { ReactComponent as Home } from "../../images/home.svg";
 import { ReactComponent as ArrowRight } from "../../images/arrow-right.svg";
 
-const defaultAttributes = {
-  width: "100%",
-  preserveAspectRatio: "xMidYMid meet",
-  style: {
-    maxWidth: "inherit",
-    maxHeight: "inherit",
-    height: "inherit",
-    strokeWidth: "2px",
-  },
-};
-
 const icons = {
-  home: <Home {...defaultAttributes} />,
-  arrowRight: <ArrowRight {...defaultAttributes} />,
+  home: <Home />,
+  arrowRight: <ArrowRight />,
 };
 
 export default icons;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,19 +1,31 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 import Button from "../Button/Button";
+import SpermImage from "../../images/pink-sperm.png";
+import ShereBig1 from "../../images/shere-big-1.png";
+import ShereBig2 from "../../images/shere-big-2.png";
+import ShereSmall1 from "../../images/shere-small-1.png";
+import ShereSmall2 from "../../images/shere-small-2.png";
 import "./Main.css";
 
 function Main() {
   return (
     <section className="main">
-      <p className="main__text">Привет,</p>
-      <h1 className="main__title">
-        Это <span className="main__title_bold">не</span>
-        <br />
-        коммерческое
-        <br />
-        задание
-      </h1>
-      <Button icon="arrowRight" text="Что дальше?" className="main__button" />
+      <div className="main__container">
+        <p className="main__text">Привет,</p>
+        <h1 className="main__title">
+          Это <span className="main__title_bold">не</span>
+          <br />
+          коммерческое
+          <br />
+          задание
+        </h1>
+        <Button icon="arrowRight" text="Что дальше?" className="main__button" />
+      </div>
+      <img className="sperm" src={SpermImage} alt="sperm" />
+      <img className="shere-big-1" src={ShereBig1} alt="shere big" />
+      <img className="shere-big-2" src={ShereBig2} alt="shere big" />
+      <img className="shere-small-1" src={ShereSmall1} alt="shere small" />
+      <img className="shere-small-2" src={ShereSmall2} alt="shere small" />
     </section>
   );
 }
